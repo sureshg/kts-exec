08efeea52fd771a12cd1c271561bd1df42b3fbf3
Suresh G <sureshg.g@gmail.com>
2022-04-18 10:08:29 -0700

    chore: test script dependency updates

--- .github/scripts/script.main.kts
+++ .github/scripts/script.main.kts
@@ -1,15 +1,15 @@
-#!/usr/bin/env -S kotlin -Xplugin=/usr/local/Cellar/kotlin/1.6.10/libexec/lib/kotlinx-serialization-compiler-plugin.jar
+#!/usr/bin/env -S kotlin -Xplugin=/usr/local/opt/kotlin/libexec/lib/kotlinx-serialization-compiler-plugin.jar
 
 // @file:Repository("https://maven.google.com")
-@file:DependsOn("io.ktor:ktor-client-core:2.0.0-beta-1")
-@file:DependsOn("io.ktor:ktor-client-cio:2.0.0-beta-1")
-@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+@file:DependsOn("io.ktor:ktor-client-core:2.0.0")
+@file:DependsOn("io.ktor:ktor-client-cio:2.0.0")
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
 @file:DependsOn("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 @file:DependsOn("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
 @file:DependsOn("org.slf4j:slf4j-simple:2.0.0-alpha7")
 // @file:Import("common.main.kts")
 
-@file:CompilerOptions("-Xopt-in=kotlin.RequiresOptIn", "-jvm-target", "1.8")
+@file:CompilerOptions("-opt-in=kotlin.RequiresOptIn", "-jvm-target", "1.8")
 @file:OptIn(ExperimentalCoroutinesApi::class, ExperimentalSerializationApi::class)
 
 import kotlinx.coroutines.*
@@ -30,6 +30,6 @@ val deserialized = Json.decodeFromString<Lang>("""{"name" : "Java", "version": "
 println(deserialized)
 
 runBlocking {
-    delay(1000)
+    delay(100)
     println("Done!")
 }
